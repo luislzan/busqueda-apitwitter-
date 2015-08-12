7bfe39b6963a86ced877b53b8667c410dcdced96
luislzan <luis_lzan@hotmail.com>
2015-08-11 19:56:45 -0500

    Create maquetacion.php

---
+++ maquetacion.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+
+<title>API Twitter</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/jquery.scrollex.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+</head>
+
+<body class="landing">
+
+		<!-- Header -->
+			<header id="header">
+				<h1 id="logo"><a href="index.php"></a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.php">Inicio</a></li>
+						<li>
+							<a href="">Paginas</a>
+							<ul>
+								<li><a href="https://dev.twitter.com">Dev Twitter</a></li>
+								<li><a href="https://apps.twitter.com">App Twitter</a></li>
+								
+									</ul>
+								</li>
+							</ul>
+						</li>
+						
+						
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="content">
+					<header>
+						<h2>Busqueda en Twitter</h2>
+						<p>Bienvenido<br />
+						</p>
+					</header>
+					<span class="image"><img src="images/h.PNG" alt="" /></span>
+				</div>
+				<a href="#four" class="goto-next scrolly">Next</a>
+			</section>
+
+	
+		<!-- Four -->
+			<section id="four" class="wrapper style1 special fade-up">
+				<div class="container">
+					<header class="major">
+						<h2>Opciones</h2>
+						<p></p>
+					</header>
+					<div class="box alt">
+						<div class="row uniform">
+							<section class="4u 6u(medium) 12u$(xsmall)">
+                            
+								<a href="busqueda.php"><span class="icon alt major fa-search"></a></span>
+								<h3>Busqueda</h3>
+								<p>Ingresa el nombre del usuario <br>para saber su informacion.</p>
+							</section>
+							
+                            	<section class="4u 6u(medium) 12u$(xsmall)">
+								<a href="logeo.php"><span class="icon alt major fa-twitter-square"></a></span>
+								<h3>Lista de Seguidores</h3>
+								<p>Muestra toda la lista de <br> los seguidores.  </p>
+							</section>
+                            
+                            	<section class="4u 6u(medium) 12u$(xsmall)">
+								<a href="inicio.php"><span class="icon alt major fa-file-text"></a></span>
+								<h3>Informacion JSON</h3>
+								<p>Muestra toda la informacion <br>en JSON</p>
+							</section>
+							
+							
+                            
+                            
+                            
+                            
+                            
+                            
+						</div>
+					</div>
+					<footer class="major">
+						<ul class="actions">
+							
+						</ul>
+					</footer>
+				</div>
+			</section>
+
+		
+
+		<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>&copy; UPT 2015</li><li>Diseñador: Luis Alberto Lozada Antonio <a href="http://html5up.net"></a></li>
+				</ul>
+			</footer>
+
+	</body>
+</html>
